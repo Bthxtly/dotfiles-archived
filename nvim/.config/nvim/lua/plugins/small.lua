4c794998753243ade4f8d3323fcbb23cbf63754c
Bthxtly <bthxtly@gmail.com>
2024-12-19 00:20:17 +0800

    add startuptime.vim

--- nvim/.config/nvim/lua/plugins/small.lua
+++ nvim/.config/nvim/lua/plugins/small.lua
@@ -4,16 +4,12 @@ return {
   { "tpope/vim-surround", lazy = false },
   { "declancm/maximize.nvim", config = true },
   { "HiPhish/rainbow-delimiters.nvim" },
+  { "dstein64/vim-startuptime" },
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
       filesystem = { window = { mappings = { ["f"] = "noop" } } },
-      window = {
-        mappings = {
-          ["<right>"] = "open",
-          ["<left>"] = "close_node",
-        },
-      },
+      window = { mappings = { ["<right>"] = "open", ["<left>"] = "close_node" } },
     },
   },
 }
