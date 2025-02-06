9c086fdbed6585ba1bd36831ef34ea9b6c0a0edb
Bthxtly <bthxtly@gmail.com>
2025-02-06 14:01:43 +0800

    change appearance

---
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,12 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  opts = {
+    options = {
+      section_separators = { left = "", right = "" },
+      component_separators = { left = "", right = "" },
+    },
+    sections = {
+      lualine_b = {},
+    },
+  },
+}
