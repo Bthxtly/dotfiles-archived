dc70598b447eef0da95cce1c5c17cb843207e879
Bthxtly <bthxtly@gmail.com>
2025-02-06 14:01:14 +0800

    disable noice.nvim

--- nvim/.config/nvim/lua/plugins/disable.lua
+++ nvim/.config/nvim/lua/plugins/disable.lua
@@ -1,4 +1,5 @@
 return {
   { "akinsho/bufferline.nvim", enabled = false, opts = { options = { show_buffer_close_icons = false } } },
   { "folke/flash.nvim", enabled = false },
+  { "folke/noice.nvim", enabled = false },
 }
