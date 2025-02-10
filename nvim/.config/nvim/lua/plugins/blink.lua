65ee37f0991f08f02a9a4db0bacf37774d1220f9
Bthxtly <bthxtly@gmail.com>
2025-02-10 21:47:53 +0800

    add nvim-scissors for snippets

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -1,24 +1,65 @@
 return {
-  "saghen/blink.cmp",
-  event = { "InsertEnter", "CmdlineEnter" },
-  opts = {
-    sources = {
-      cmdline = function()
-        local type = vim.fn.getcmdtype()
-        -- Search forward and backward
-        if type == "/" or type == "?" then
-          return { "buffer" }
-        end
-        -- Commands
-        if type == ":" then
-          return { "cmdline" }
-        end
-        return {}
-      end,
+  -- code completion
+  {
+    "saghen/blink.cmp",
+    build = "cargo build --release",
+    event = { "InsertEnter", "CmdlineEnter" },
+    opts = {
+
+      sources = {
+        providers = {
+          snippets = {
+            opts = {
+              search_paths = { vim.fn.stdpath("config") .. "/snippets" },
+            },
+          },
+        },
+
+        cmdline = function()
+          local type = vim.fn.getcmdtype()
+          -- Search forward and backward
+          if type == "/" or type == "?" then
+            return { "buffer" }
+          end
+          -- Commands
+          if type == ":" then
+            return { "cmdline" }
+          end
+          return {}
+        end,
+      },
+
+      keymap = {
+        ["<C-y>"] = { "accept", "fallback" },
+        ["<CR>"] = {},
+      },
     },
-    keymap = {
-      ["<C-y>"] = { "accept", "fallback" },
-      ["<CR>"] = {},
+  },
+
+  -- add and edit snippets easily
+  {
+    "chrisgrieser/nvim-scissors",
+    keys = {
+      {
+        "<leader>se",
+        function()
+          require("scissors").editSnippet()
+        end,
+        desc = "Edit snippet",
+        mode = "n",
+      },
+      {
+        "<leader>sn",
+        function()
+          require("scissors").addNewSnippet()
+        end,
+        desc = "Add new snippet",
+        mode = { "n", "v" },
+      },
+    },
+    -- dependencies = "nvim-telescope/telescope.nvim",
+    opts = {
+      snippetDir = vim.fn.stdpath("config") .. "/snippets",
     },
   },
 }
