1a6b03547400dd085f68b0530414731bd21c8423
Bthxtly <bthxtly@gmail.com>
2025-02-10 21:48:23 +0800

    add <leader>su for undotree

--- nvim/.config/nvim/lua/plugins/small.lua
+++ nvim/.config/nvim/lua/plugins/small.lua
@@ -3,13 +3,28 @@ return {
   { "tpope/vim-surround" },
   { "HiPhish/rainbow-delimiters.nvim" },
   { "nanotee/zoxide.vim", lazy = true, cmd = { "Z" } },
-  { "mbbill/undotree", lazy = true, cmd = { "UndotreeToggle" } },
   { "declancm/maximize.nvim", lazy = true, cmd = { "Maximize" } },
+
   {
     "AndrewRadev/sideways.vim",
     lazy = true,
     cmd = { "SidewaysLeft", "SidewaysRight" },
   },
+
+  {
+    "mbbill/undotree",
+    lazy = true,
+    -- cmd = { "UndotreeToggle" },
+    keys = {
+      {
+        "<leader>su",
+        "<cmd>UndotreeToggle<cr>",
+        mode = "n",
+        desc = "Undotree",
+      },
+    },
+  },
+
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
