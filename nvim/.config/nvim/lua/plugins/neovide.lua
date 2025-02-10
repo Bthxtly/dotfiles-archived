f4ba7b62114bc4f32bf44ab0c09453f0dde310a8
Bthxtly <bthxtly@gmail.com>
2025-02-10 21:47:13 +0800

    remove shadow at edge

--- nvim/.config/nvim/lua/plugins/neovide.lua
+++ nvim/.config/nvim/lua/plugins/neovide.lua
@@ -1,12 +1,7 @@
 if vim.g.neovide then
   vim.o.guifont = "ComicShannsMono Nerd Font:h12" -- text below applies for VimScript
+  vim.g.neovide_floating_shadow = false
   vim.keymap.set({ "n", "i" }, "<C-S-V>", '<ESC>"+p', { silent = true })
-  return {
-    "LazyVim/LazyVim",
-    opts = {
-      -- colorscheme = "strawberry-light",
-    },
-  }
 end
 
 return {}
