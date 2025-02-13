db2799511ea624be4e5ea73f0200ee69c637ab84
Bthxtly <bthxtly@gmail.com>
2025-02-13 13:36:04 +0800

    fix markdown headings highlight

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -4,7 +4,6 @@ return {
   -- add gruvbox
   {
     "ellisonleao/gruvbox.nvim",
-    enabled = true,
     priority = 1000,
     config = true,
     opts = {
@@ -19,6 +18,15 @@ return {
         folds = true,
       },
       contrast = "soft",
+      overrides = {
+        -- Markdown Header Background Overrides with Foreground Colors
+        ["@markup.heading.1.markdown"] = { fg = "#fb4934", bg = "", bold = true },
+        ["@markup.heading.2.markdown"] = { fg = "#fabd2f", bg = "", bold = true },
+        ["@markup.heading.3.markdown"] = { fg = "#b8bb26", bg = "", bold = true },
+        ["@markup.heading.4.markdown"] = { fg = "#8ec07c", bg = "", bold = true },
+        ["@markup.heading.5.markdown"] = { fg = "#83a598", bg = "", bold = true },
+        ["@markup.heading.6.markdown"] = { fg = "#d3869b", bg = "", bold = true },
+      },
     },
   },
 
@@ -30,7 +38,6 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      -- colorscheme = "strawberry-light",
       colorscheme = "gruvbox",
       icons = {
         kinds = {
