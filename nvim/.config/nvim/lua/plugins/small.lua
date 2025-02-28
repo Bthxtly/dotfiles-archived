6fc5cf140727e17887ce8f6d7341ed8c56075bee
Bthxtly <bthxtly@gmail.com>
2025-02-28 14:51:12 +0800

    html-style markdown comment

--- nvim/.config/nvim/lua/plugins/small.lua
+++ nvim/.config/nvim/lua/plugins/small.lua
@@ -4,7 +4,13 @@ return {
   { "HiPhish/rainbow-delimiters.nvim" },
   { "nanotee/zoxide.vim", lazy = true, cmd = { "Z" } },
   { "declancm/maximize.nvim", lazy = true, cmd = { "Maximize" } },
-  { "folke/ts-comments.nvim", event = "VeryLazy", opts = { lang = { markdown = "<!-- %s -->" } } },
+
+  {
+    "folke/ts-comments.nvim",
+    lazy = true,
+    event = "VeryLazy",
+    opts = { lang = { markdown = "<!-- %s -->" } },
+  },
 
   {
     "AndrewRadev/sideways.vim",
