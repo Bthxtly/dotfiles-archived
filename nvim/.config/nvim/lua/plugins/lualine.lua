5401e5a1ca50b62edc20ef6da67a21177ebd9497
Bthxtly <bthxtly@gmail.com>
2025-02-28 14:50:30 +0800

    make lualine fancy

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,12 +1,25 @@
 return {
   "nvim-lualine/lualine.nvim",
+  dependencies = { "bwpge/lualine-pretty-path" },
   opts = {
     options = {
+      theme = "auto",
       section_separators = { left = "", right = "" },
       component_separators = { left = "", right = "" },
+      disabled_filetypes = { statusline = { "snacks_dashboard", "Outline" } },
     },
     sections = {
-      lualine_b = {},
+      lualine_a = {},
+      lualine_c = { "pretty_path" },
+      lualine_y = {
+        { "progress", separator = " ", padding = { left = 1, right = 0 } },
+        { "location", padding = { left = 0, right = 1 } },
+      },
+      lualine_z = {
+        function()
+          return " " .. os.date("%R")
+        end,
+      },
     },
   },
 }
