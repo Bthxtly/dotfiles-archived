4ed99e0db750d57f08684ec646bee2dbc5042aab
Bthxtly <bthxtly@gmail.com>
2025-03-04 23:34:12 +0800

    sort

--- nvim/.config/nvim/colors/strawberry.lua
+++ nvim/.config/nvim/colors/strawberry.lua
@@ -1,13 +1,14 @@
+# strawberry light, inspired by https://github.com/bringsrain/strawberry
 require("mini.base16").setup({
   palette = {
-    base07 = "#2b1d24",
-    base06 = "#3b2c33",
-    base05 = "#75616b",
-    base04 = "#8a7680",
-    base03 = "#9e8b95",
-    base02 = "#b5a3ac",
-    base01 = "#f0dde6",
     base00 = "#fff0f7",
+    base01 = "#f0dde6",
+    base02 = "#b5a3ac",
+    base03 = "#9e8b95",
+    base04 = "#8a7680",
+    base05 = "#75616b",
+    base06 = "#3b2c33",
+    base07 = "#2b1d24",
     base08 = "#f55050",
     base09 = "#e06a26",
     base0A = "#d4ac35",
