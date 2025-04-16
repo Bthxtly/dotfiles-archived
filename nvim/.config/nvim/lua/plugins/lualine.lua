6beb66e0ca0d8b91d04d247eab7d82a3990df294
Bthxtly <bthxtly@gmail.com>
2025-04-16 09:40:52 +0800

    integrate trouble.nvim

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,31 +1,57 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  opts = {
-    options = {
-      theme = "auto",
-      section_separators = { left = "", right = "" },
-      component_separators = { left = "", right = "" },
-      disabled_filetypes = { statusline = { "snacks_dashboard", "Outline" } },
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = {
+      "folke/trouble.nvim",
     },
-    sections = {
-      lualine_a = {},
-      lualine_y = {
-        { "progress", separator = " ", padding = { left = 1, right = 0 } },
-        { "location", padding = { left = 0, right = 1 } },
-      },
-      lualine_z = {
-        function()
-          return " " .. os.date("%R")
-        end,
-      },
-    },
-    inactive_sections = {
-      lualine_a = {},
-      lualine_b = {},
-      lualine_c = { "filename" },
-      lualine_x = { "location" },
-      lualine_y = {},
-      lualine_z = {},
-    },
+    opts = function()
+      local opts = {
+        options = {
+          theme = "auto",
+          section_separators = { left = "", right = "" },
+          component_separators = { left = "", right = "" },
+          disabled_filetypes = { statusline = { "snacks_dashboard", "Outline" } },
+        },
+        sections = {
+          lualine_a = {},
+          lualine_b = { "branch", "diff", "diagnostics" },
+          lualine_c = { "filename" },
+          lualine_x = { "filetype" },
+          lualine_y = {
+            { "progress", separator = " ", padding = { left = 1, right = 0 } },
+            { "location", padding = { left = 0, right = 1 } },
+          },
+          lualine_z = {
+            function()
+              return " " .. os.date("%R")
+            end,
+          },
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = { "filename" },
+          lualine_x = { "location" },
+          lualine_y = {},
+          lualine_z = {},
+        },
+      }
+
+      local trouble = require("trouble")
+      local symbols = trouble.statusline({
+        mode = "lsp_document_symbols",
+        groups = {},
+        title = false,
+        filter = { range = true },
+        format = "{kind_icon}{symbol.name:Normal}",
+        hl_group = "lualine_c_normal",
+      })
+      table.insert(opts.sections.lualine_c, {
+        symbols.get,
+        cond = symbols.has,
+      })
+
+      return opts
+    end,
   },
 }
