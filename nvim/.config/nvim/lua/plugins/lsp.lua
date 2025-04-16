0ec73fdd476e3df007dea1ca8d58a2ccd7063785
Bthxtly <bthxtly@gmail.com>
2025-04-16 09:40:38 +0800

    toggle inlay_hint

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,4 +1,7 @@
 vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, { desc = "rename" })
+vim.keymap.set("n", "<leader>uh", function()
+  vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+end, { desc = "toggle inlay hints" })
 
 return {
   {
@@ -28,7 +31,7 @@ return {
           source = "if_many",
           prefix = "●",
         },
-        virtual_lines = { current_line = true },
+        -- virtual_lines = { current_line = true },
         underline = true,
         update_in_insert = false,
         severity_sort = true,
@@ -43,82 +46,6 @@ return {
       })
       return opts
     end,
-    -- -- Enable this to enable the builtin LSP inlay hints on Neovim >= 0.10.0
-    -- -- Be aware that you also will need to properly configure your LSP server to
-    -- -- provide the inlay hints.
-    -- inlay_hints = {
-    --   enabled = true,
-    --   exclude = { "vue" }, -- filetypes for which you don't want to enable inlay hints
-    -- },
-    -- -- Enable this to enable the builtin LSP code lenses on Neovim >= 0.10.0
-    -- -- Be aware that you also will need to properly configure your LSP server to
-    -- -- provide the code lenses.
-    -- codelens = {
-    --   enabled = false,
-    -- },
-    -- -- add any global capabilities here
-    -- capabilities = {
-    --   workspace = {
-    --     fileOperations = {
-    --       didRename = true,
-    --       willRename = true,
-    --     },
-    --   },
-    -- },
-    -- -- options for vim.lsp.buf.format
-    -- -- `bufnr` and `filter` is handled by the LazyVim formatter,
-    -- -- but can be also overridden when specified
-    -- format = {
-    --   formatting_options = nil,
-    --   timeout_ms = nil,
-    -- },
-    -- -- LSP Server Settings
-    -- ---@type lspconfig.options
-    -- servers = {
-    --   lua_ls = {
-    --     -- mason = false, -- set to false if you don't want this server to be installed with mason
-    --     -- Use this to add any additional keymaps
-    --     -- for specific lsp servers
-    --     -- ---@type LazyKeysSpec[]
-    --     -- keys = {},
-    --     settings = {
-    --       Lua = {
-    --         workspace = {
-    --           checkThirdParty = false,
-    --         },
-    --         codeLens = {
-    --           enable = true,
-    --         },
-    --         completion = {
-    --           callSnippet = "Replace",
-    --         },
-    --         doc = {
-    --           privateName = { "^_" },
-    --         },
-    --         hint = {
-    --           enable = true,
-    --           setType = false,
-    --           paramType = true,
-    --           paramName = "Disable",
-    --           semicolon = "Disable",
-    --           arrayIndex = "Disable",
-    --         },
-    --       },
-    --     },
-    --   },
-    -- },
-    -- -- you can do any additional lsp server setup here
-    -- -- return true if you don't want this server to be setup with lspconfig
-    -- ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
-    -- setup = {
-    --   -- example to setup with typescript.nvim
-    --   -- tsserver = function(_, opts)
-    --   --   require("typescript").setup({ server = opts })
-    --   --   return true
-    --   -- end,
-    --   -- Specify * to use this function as a fallback for any server
-    --   -- ["*"] = function(server, opts) end,
-    -- },
 
     config = function()
       vim.lsp.inlay_hint.enable(true)
@@ -156,6 +83,9 @@ return {
 
       -- json
       require("lspconfig").jsonls.setup({})
+
+      -- cmake
+      require("lspconfig").cmake.setup({})
     end,
   },
 }
