0676763e0d360c87a11654b2e95d343b3f49a837
Bthxtly <bthxtly@gmail.com>
2025-05-03 23:00:59 +0800

    combine small plugins

--- nvim/.config/nvim/lua/plugins/marks.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "chentoast/marks.nvim",
-  -- config = load_config("tools.marks"),
-  event = { "BufReadPre", "BufNewFile" },
-  opts = {
-    builtin_marks = { ".", "<", ">", "^" },
-  },
-}
--- nvim/.config/nvim/lua/plugins/small.lua
+++ nvim/.config/nvim/lua/plugins/small.lua
@@ -1,9 +1,17 @@
 return {
   { "tpope/vim-repeat" },
+  { "tpope/vim-fugitive" },
   { "j-hui/fidget.nvim" },
   { "tpope/vim-surround" },
   { "HiPhish/rainbow-delimiters.nvim" },
+  { "dnlhc/glance.nvim", lazy = true, cmd = "Glance" },
   { "declancm/maximize.nvim", lazy = true, cmd = { "Maximize" } },
+
+  {
+    "chentoast/marks.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    opts = { builtin_marks = { ".", "^" } },
+  },
 
   {
     "folke/ts-comments.nvim",
