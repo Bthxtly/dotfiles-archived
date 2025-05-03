948955db438fbecf9cfc7085aaf9d33661da016f
Bthxtly <bthxtly@gmail.com>
2025-05-03 23:01:14 +0800

    disable lualine for dap-view

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -11,7 +11,7 @@ return {
           section_separators = { left = "", right = "" },
           component_separators = { left = "", right = "" },
           disabled_filetypes = {
-            statusline = { "snacks_dashboard", "Outline", "dap-view", "dap-view-term" },
+            statusline = { "snacks_dashboard", "Outline", "dap-view", "dap-repl", "dap-view-term" },
           },
         },
         sections = {
