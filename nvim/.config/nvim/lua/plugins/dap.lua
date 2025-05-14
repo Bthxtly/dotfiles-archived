4845f3d7ada1670843474f3a0e10f86b20244256
Bthxtly <bthxtly@gmail.com>
2025-05-14 23:24:13 +0800

    debugmaster is all you need

--- nvim/.config/nvim/lua/plugins/dap.lua
+++ nvim/.config/nvim/lua/plugins/dap.lua
@@ -2,29 +2,7 @@ return {
   {
     "mfussenegger/nvim-dap",
     dependencies = {
-      { "igorlfs/nvim-dap-view", opts = {} },
       { "mfussenegger/nvim-dap-python" },
-    },
-
-    -- stylua: ignore
-    keys = {
-      { "<leader>da", function() require("dap").continue({ before = get_args }) end, desc = "Run with Args" },
-      { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" },
-      { "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = "Breakpoint Condition" },
-      { "<leader>dc", function() require("dap").continue() end, desc = "Run/Continue" },
-      { "<leader>dC", function() require("dap").run_to_cursor() end, desc = "Run to Cursor" },
-      { "<leader>dg", function() require("dap").goto_() end, desc = "Go to Line (No Execute)" },
-      { "<leader>di", function() require("dap").step_into() end, desc = "Step Into" },
-      { "<leader>dl", function() require("dap").run_last() end, desc = "Run Last" },
-      { "<leader>dn", function() require("dap").down() end, desc = "Next" },
-      { "<leader>do", function() require("dap").step_out() end, desc = "Step Out" },
-      { "<leader>dO", function() require("dap").step_over() end, desc = "Step Over" },
-      { "<leader>dP", function() require("dap").pause() end, desc = "Pause" },
-      { "<leader>dp", function() require("dap").up() end, desc = "Previous" },
-      { "<leader>dr", function() require("dap").repl.toggle() end, desc = "Toggle REPL" },
-      { "<leader>ds", function() require("dap").session() end, desc = "Session" },
-      { "<leader>dt", function() require("dap").terminate() end, desc = "Terminate" },
-      { "<leader>dw", function() require("dap.ui.widgets").hover() end, desc = "Widgets" },
     },
 
     config = function()
@@ -58,6 +36,16 @@ return {
           },
         }
       end
+    end,
+  },
+
+  {
+    "miroshQa/debugmaster.nvim",
+    config = function()
+      local dm = require("debugmaster")
+      -- make sure you don't have any other keymaps that starts with "<leader>d" to avoid delay
+      vim.keymap.set({ "n", "v" }, "<leader>n", dm.mode.toggle, { nowait = true })
+      vim.keymap.set("t", "<C-/>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
     end,
   },
 
