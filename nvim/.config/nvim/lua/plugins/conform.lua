5da99c55692d41e8f8bae74b6b246ffcf3f2bc80
Bthxtly <bthxtly@gmail.com>
2025-05-16 00:14:09 +0800

    clean

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,23 +1,3 @@
--- toggle format on save
-vim.api.nvim_create_user_command("FormatDisable", function(args)
-  if args.bang then
-    -- FormatDisable! will disable formatting just for this buffer
-    vim.b.disable_autoformat = true
-  else
-    vim.g.disable_autoformat = true
-  end
-end, {
-  desc = "Disable autoformat-on-save",
-  bang = true,
-})
-
-vim.api.nvim_create_user_command("FormatEnable", function()
-  vim.b.disable_autoformat = false
-  vim.g.disable_autoformat = false
-end, {
-  desc = "Re-enable autoformat-on-save",
-})
-
 return {
   "stevearc/conform.nvim",
   dependencies = { "mason.nvim" },
@@ -53,17 +33,6 @@ return {
     ---@type table<string, conform.FormatterConfigOverride|fun(bufnr: integer): nil|conform.FormatterConfigOverride>
     formatters = {
       injected = { options = { ignore_errors = true } },
-      -- # Example of using dprint only when a dprint.json file is present
-      -- dprint = {
-      --   condition = function(ctx)
-      --     return vim.fs.find({ "dprint.json" }, { path = ctx.filename, upward = true })[1]
-      --   end,
-      -- },
-      --
-      -- # Example of using shfmt with extra args
-      -- shfmt = {
-      --   prepend_args = { "-i", "2", "-ci" },
-      -- },
     },
   },
 }
--- nvim/.config/nvim/lua/plugins/origami.lua
+++ nvim/.config/nvim/lua/plugins/origami.lua
@@ -1,12 +1,12 @@
--- stylua: ignore
-vim.keymap.set("n", "<Left>", function() require("origami").h() end)
--- stylua: ignore
-vim.keymap.set("n", "<Right>", function() require("origami").l() end)
-
 return {
   "chrisgrieser/nvim-origami",
   event = "VeryLazy",
   opts = {
     foldKeymaps = { setup = true },
   },
+-- stylua: ignore
+  config = function()
+    vim.keymap.set("n", "<Left>", function() require("origami").h() end)
+    vim.keymap.set("n", "<Right>", function() require("origami").l() end)
+  end,
 }
