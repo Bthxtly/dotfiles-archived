48da70ae40625ecc9d3d60735fdcbd31b0aeb389
Bthxtly <bthxtly@gmail.com>
2025-09-11 20:01:51 +0800

    nvim: setup foldtext

--- nvim/.config/nvim/lua/plugins/origami.lua
+++ nvim/.config/nvim/lua/plugins/origami.lua
@@ -1,12 +1,19 @@
 return {
   "chrisgrieser/nvim-origami",
   event = "VeryLazy",
-  opts = {
-    foldKeymaps = { setup = true },
-  },
--- stylua: ignore
   config = function()
+    require("origami").setup({
+      foldtext = {
+        padding = 2,
+        lineCount = {
+          template = "󰘖  %d lines ",
+        },
+      },
+    })
+
+    -- stylua: ignore
     vim.keymap.set("n", "<Left>", function() require("origami").h() end)
+    -- stylua: ignore
     vim.keymap.set("n", "<Right>", function() require("origami").l() end)
   end,
 }
