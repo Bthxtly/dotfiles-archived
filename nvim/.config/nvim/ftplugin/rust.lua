6edf3968f1fd14ec905329d897b86a30f1ca358a
Bthxtly <bthxtly@gmail.com>
2025-10-30 17:58:22 +0800

    nvim: add more abbreviation to Rust

--- nvim/.config/nvim/ftplugin/rust.lua
+++ nvim/.config/nvim/ftplugin/rust.lua
@@ -1,13 +1,22 @@
 local abbr = {
+  ie = "i8",
+  iah = "i16",
   isr = "i32",
   iht = "i64",
+
+  fe = "f8",
+  fah = "f16",
   fsr = "f32",
   fht = "f64",
+
+  ue = "u8",
+  uah = "u16",
   usr = "u32",
   uht = "u64",
-  ue = "u8",
 }
 
 for ab, type in pairs(abbr) do
   vim.cmd("iabbr " .. ab .. " " .. type)
 end
+
+vim.cmd("iabbr ;; ::")
