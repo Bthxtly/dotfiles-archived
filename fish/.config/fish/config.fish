8a20a14fbdd24e1bca22bf884c68700cdc9832c4
Bthxtly <bthxtly@gmail.com>
2025-11-06 18:19:38 +0800

    fish: add niri completion

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,20 +1,9 @@
-# prime-run Nvidia GPU if the external monitor is connected
-if status is-login
-    if test (tty) = /dev/tty1
-        if lsusb | grep -q "Genesys Logic, Inc. Billboard Device"
-            # run Hyprland with env var
-            env AQ_DRM_DEVICES="/dev/dri/card0:/dev/dri/card1" Hyprland
-        else
-            Hyprland
-        end
-    end
-end
-
 if status is-interactive
     # shell integrations
     fzf --fish | source
     thefuck --alias | source
     zoxide init fish | source
+    niri completions fish | source
 
     # clear greeting message
     set -U fish_greeting
